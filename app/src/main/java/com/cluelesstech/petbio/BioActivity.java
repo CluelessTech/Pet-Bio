88a2f3145845e60d600e3620b2b43d802573a08d
Clueless Tech <gpriyank42@gmail.com>
2020-08-30 14:19:38 +0530

    Pet Bio
    Signed-off-by: Priyank Gandhi <gpriyank42@gmail.com>

---
+++ app/src/main/java/com/cluelesstech/petbio/BioActivity.java
@@ -0,0 +1,49 @@
+package com.cluelesstech.petbio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class BioActivity extends AppCompatActivity {
+
+    private ImageView petImageView;
+    private TextView petName, petBio;
+    private Bundle extras;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_bio);
+
+
+        petImageView = findViewById(R.id.imageViewID);
+        petName = findViewById(R.id.nameID);
+        petBio = findViewById(R.id.bioTextID);
+
+        extras = getIntent().getExtras();
+        if(extras != null){
+            String name = extras.getString("Name");
+            String bio = extras.getString("Bio");
+            setup(name,bio);
+        }
+    }
+
+    private void setup(String name, String bio) {
+        if(name.equals("Kitty")){
+            petImageView.setImageDrawable(getResources().getDrawable(R.drawable.icon_lg_cat));
+            petName.setText(name);
+            petBio.setText(bio);
+        }else{
+            petImageView.setImageDrawable(getResources().getDrawable(R.drawable.icon_lg_dog));
+            petName.setText(name);
+            petBio.setText(bio);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/cluelesstech/petbio/MainActivity.java
@@ -0,0 +1,50 @@
+package com.cluelesstech.petbio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView catView, DogView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        catView = findViewById(R.id.catID);
+        DogView = findViewById(R.id.dogID);
+
+        catView.setOnClickListener(this);
+        DogView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.catID:
+                Intent catIntent = new Intent(MainActivity.this,BioActivity.class);
+                catIntent.putExtra("Name","Kitty");
+                catIntent.putExtra("Bio","Great Cat. Loves people and meows a lot");
+                startActivity(catIntent);
+                Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.dogID:
+                Intent dogIntent = new Intent(MainActivity.this, BioActivity.class);
+                dogIntent.putExtra("Name", "Dufus");
+                dogIntent.putExtra("Bio", "Great Dog. Loves People, barks and eats a lot");
+                startActivity(dogIntent);
+                Toast.makeText(MainActivity.this,"Clicked",Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
